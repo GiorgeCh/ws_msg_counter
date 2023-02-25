b0c630528b2b117e04d5d547ddeceb34de20cbe3
Giorge Chernyakov <88615629+gidiChernyakov@users.noreply.github.com>
2023-02-25 17:53:01 +0200

    Updates

---
+++ precache-manifest.54a3d19efc6db4a664df1b19b40ca17c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb63f286d71397857618",
+    "url": "/static/css/main.0e58d508.chunk.css"
+  },
+  {
+    "revision": "cb63f286d71397857618",
+    "url": "/static/js/main.cb63f286.chunk.js"
+  },
+  {
+    "revision": "fa677b773aaf13c72ca8",
+    "url": "/static/js/1.fa677b77.chunk.js"
+  },
+  {
+    "revision": "4764495a313b055ac6e2",
+    "url": "/static/js/2.4764495a.chunk.js"
+  },
+  {
+    "revision": "927d7cfaacafd359ffba",
+    "url": "/static/js/runtime~main.927d7cfa.js"
+  },
+  {
+    "revision": "f57c894f707f550658c7406141233e66",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1fd324b89eca578f45799526bc8b7b09.js"
+  "/precache-manifest.54a3d19efc6db4a664df1b19b40ca17c.js"
 );
 
 workbox.clientsClaim();
