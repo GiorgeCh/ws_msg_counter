e137051a914f2fb2ac2a2017285c74c18d6a632a
Giorge Chernyakov <88615629+gidiChernyakov@users.noreply.github.com>
2023-02-25 16:48:08 +0200

    Updates

---
+++ precache-manifest.08a4434ff5eccffb77b5f60856213088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b553cf94e97abe23cb1",
+    "url": "/static/css/main.52a0885f.chunk.css"
+  },
+  {
+    "revision": "5b553cf94e97abe23cb1",
+    "url": "/static/js/main.5b553cf9.chunk.js"
+  },
+  {
+    "revision": "6f6f4986655de22842af",
+    "url": "/static/js/1.6f6f4986.chunk.js"
+  },
+  {
+    "revision": "79c907a05cf4ed19e9f5",
+    "url": "/static/js/2.79c907a0.chunk.js"
+  },
+  {
+    "revision": "15039e497f60efd69200",
+    "url": "/static/js/runtime~main.15039e49.js"
+  },
+  {
+    "revision": "5658980e2398b4b4f2f10bdb1112587c",
+    "url": "/index.html"
+  }
+];
